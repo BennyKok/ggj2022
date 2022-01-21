bac5954f532821ee039df32790ad64ccf387fc97
JackyWu <ttca914@gmail.com>
2022-01-22 03:21:14 +0800

    added animation

--- Assets/JackyStuffs/DayNight/PlayerDayNightController.cs
+++ Assets/JackyStuffs/DayNight/PlayerDayNightController.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
+using System.Threading;
 
 public class PlayerDayNightController : MonoBehaviour
 {
     public static PlayerDayNightController Instance;
+    public Animator staffAnimator;
+    public SpriteRenderer orbSpriteRenderer;
+    public Sprite dayOrb, nightOrb;
     public bool isCooldowning;
 
     private void Awake()
     {
         Instance = this;
+        DayNightSwitcher.Instance.SwitchToSpecificDayNight(DayNightSwitcher.DayNightEnum.day);
     }
 
     void Update()
@@ -18,8 +23,25 @@ public class PlayerDayNightController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && !isCooldowning)
         {
             isCooldowning = true;
-            WaitCooldown(3);
-            DayNightSwitcher.Instance.SwitchDayNight();
+            DayNightSwitcher.DayNightEnum dayNightEnum = DayNightSwitcher.Instance.SwitchDayNight();
+            if (dayNightEnum == DayNightSwitcher.DayNightEnum.day)
+            {
+                orbSpriteRenderer.sprite = dayOrb;
+            }
+            else
+            {
+                orbSpriteRenderer.sprite = nightOrb;
+            }
+            if (staffAnimCancelSource != null)
+            {
+                staffAnimCancelSource.Cancel();
+                staffAnimCancelSource.Dispose();
+                staffAnimCancelSource = null;
+            }
+            staffAnimCancelSource = new CancellationTokenSource();
+            PerformStaffAnimation(staffAnimCancelSource.Token);
+
+            WaitCooldown(1.5f);
         }
     }
 
@@ -27,5 +49,21 @@ public class PlayerDayNightController : MonoBehaviour
     {
         await Task.Delay((int)(seconds * 1000));
         isCooldowning = false;
+    }
+
+    private CancellationTokenSource staffAnimCancelSource = null;
+    private async void PerformStaffAnimation(CancellationToken token)
+    {
+        try
+        {
+            staffAnimator.SetBool("isWanding", true);
+            await Task.Delay((int)(staffAnimator.GetCurrentAnimatorStateInfo(0).length * 1000), token);
+            staffAnimator.SetBool("isWanding", false);
+        }
+        catch (System.OperationCanceledException) when (token.IsCancellationRequested)
+        {
+            staffAnimator.SetBool("isWanding", false);
+            return;
+        }
     }
 }
