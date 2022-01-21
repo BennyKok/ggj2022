7aadca1fa38cf745be9d99f046af86a076e3e8b4
JackyWu <ttca914@gmail.com>
2022-01-22 01:37:40 +0800

    cooldown

--- Assets/JackyStuffs/DayNight/PlayerDayNightController.cs
+++ Assets/JackyStuffs/DayNight/PlayerDayNightController.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class PlayerDayNightController : MonoBehaviour
 {
-    
+    public static PlayerDayNightController Instance;
+    public bool isCooldowning;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !isCooldowning)
         {
+            isCooldowning = true;
+            WaitCooldown(3);
             DayNightSwitcher.Instance.SwitchDayNight();
         }
+    }
+
+    private async void WaitCooldown(float seconds)
+    {
+        await Task.Delay((int)(seconds * 1000));
+        isCooldowning = false;
     }
 }
