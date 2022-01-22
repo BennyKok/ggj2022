6bb2c884cef3e67b7ab58890f93ed8f5c477f313
JackyWu <ttca914@gmail.com>
2022-01-23 00:01:41 +0800

    egg spawner

---
+++ Assets/JackyStuffs/ExplosiveEgg/EggSpawner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggSpawner : MonoBehaviour
+{
+    public GameObject explosiveEggPrefab;
+
+    public float explodeSeconds, spawnSeconds;
+    public int eggLimited;
+
+    [SerializeField]private int eggSpawned = 0;
+
+    public List<GameObject> eggs;
+
+    private void Start()
+    {
+        eggs = new List<GameObject>();
+        DayNightSwitcher.Instance.SwitchDayNightEvent += OnSwitch;
+    }
+
+    private void OnDestroy()
+    {
+        DayNightSwitcher.Instance.SwitchDayNightEvent -= OnSwitch;
+    }
+
+    private void OnSwitch()
+    {
+        StopAllCoroutines();
+
+        if (DayNightSwitcher.Instance.currentDayNight == DayNightSwitcher.DayNightEnum.night)
+        {
+            foreach (GameObject g in eggs)
+            {
+                if (g != null)
+                    Destroy(g);
+            }
+            eggs.Clear();
+            eggSpawned = 0;
+
+            StartCoroutine(SpawnExplosiveEgg());
+        }
+    }
+
+    private IEnumerator SpawnExplosiveEgg()
+    {
+        while (true)
+        {
+            if (eggSpawned < eggLimited)
+            {
+                GameObject temp = Instantiate(explosiveEggPrefab, transform.position, Quaternion.identity);
+                eggs.Add(temp);
+                temp.GetComponent<ExplosiveEgg>().explodeSeconds = explodeSeconds;
+                eggSpawned++;
+            }
+            else
+            {
+                yield return new WaitUntil(() => eggSpawned < eggLimited);
+            }
+            yield return new WaitForSeconds(spawnSeconds);
+        }
+    }
+}
