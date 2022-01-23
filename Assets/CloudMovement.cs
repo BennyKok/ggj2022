83a04af4ea975af261963f7ee07279516e83253f
Edwin Wong <ew20128@gmail.com>
2022-01-23 17:46:44 +0800

    daynight cloud

--- Assets/CloudMovement.cs
+++ Assets/CloudMovement.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-public class CloudMovement : MonoBehaviour
+public class CloudMovement : DayNightComponent
 {
     public List<GameObject> clouds;
     public float time = 1;
 
-    void Start()
+    protected override void OnDayNightSwitch(bool isLight)
     {
+        gameObject.SetActive(isLight);
     }
 
     void Update()
     {
         Cloud();
     }
-
+    
     public void Cloud()
     {
         foreach (var cloud in clouds)
         {
+            cloud.transform.DOKill();
             var c = cloud.transform.position;
             double a = (Mathf.Sin(Time.time * time) + c.y);
-            cloud.transform.DOKill();
             cloud.transform.DOMoveY((float) a, 1f);
         }
     }
