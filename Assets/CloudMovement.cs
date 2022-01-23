a0511d61383b4da6a707c99c60e6cdeb7c030b61
Edwin Wong <ew20128@gmail.com>
2022-01-23 17:37:22 +0800

    cloud

--- Assets/CloudMovement.cs
+++ Assets/CloudMovement.cs
@@ -6,10 +6,6 @@ using UnityEngine;
 public class CloudMovement : MonoBehaviour
 {
     public List<GameObject> clouds;
-    public float minimum = -1.0F;
-    public float maximum = 1.0F;
-
-    static float t = 0.0f;
     public float time = 1;
 
     void Start()
@@ -18,22 +14,15 @@ public class CloudMovement : MonoBehaviour
 
     void Update()
     {
-        // if (Time.time - time < 0.2) return;
         Cloud();
-    }
-
-    public void DTime()
-    {
-        // time = Time.time;
     }
 
     public void Cloud()
     {
         foreach (var cloud in clouds)
         {
-            double a = (Mathf.Sin(Time.time * time) + 1) / 2.0;
-
             var c = cloud.transform.position;
+            double a = (Mathf.Sin(Time.time * time) + c.y);
             cloud.transform.DOKill();
             cloud.transform.DOMoveY((float) a, 1f);
         }
